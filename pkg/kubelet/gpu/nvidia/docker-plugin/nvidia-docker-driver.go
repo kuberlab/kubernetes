6814f92716493ae7f17c9dad04bc57b5e4fd419a
Alexander Gunin <guninalexander@gmail.com>
2017-05-10 21:55:48 -0700

    mount gpu

--- pkg/kubelet/gpu/nvidia/docker-plugin/nvidia-docker-driver.go
+++ pkg/kubelet/gpu/nvidia/docker-plugin/nvidia-docker-driver.go
@@ -17,18 +17,18 @@ type dockerArgs struct {
 func GetNVIDIADriverMount(pluginAddress string) (binding *kubecontainer.Mount, driver string, err error) {
 	resp, respErr := http.DefaultClient.Get(pluginAddress)
 	if respErr != nil {
-		err = fmt.Sprintf("Failed read GPU driver mount points from nvidia-docker-plugin: %v", respErr)
+		err = fmt.Errorf("Failed read GPU driver mount points from nvidia-docker-plugin: %v", respErr)
 		return
 	}
 	if resp.StatusCode != http.StatusOK {
-		err = fmt.Sprintf("Failed read GPU driver mount points from nvidia-docker-plugin response status = %s", resp.Status)
+		err = fmt.Errorf("Failed read GPU driver mount points from nvidia-docker-plugin response status = %s", resp.Status)
 		return
 	}
 	defer resp.Body.Close()
 	decoder := json.NewDecoder(resp.Body)
 	args := dockerArgs{}
 	if derr := decoder.Decode(&args); derr != nil {
-		err = fmt.Sprintf("Failed read nvidia-docker-plugin response: %v", derr)
+		err = fmt.Errorf("Failed read nvidia-docker-plugin response: %v", derr)
 		return
 	}
 	if len(args.Volumes) > 0 {
